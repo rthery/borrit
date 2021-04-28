cc3ecb0c8cea9bae65230614f32a59c23cd9c8f1
Romain Théry <romain.thery@live.fr>
2021-04-28 20:43:21 +0200

    fix: Add validation to the google app script url

--- Editor/Database/GoogleAppScript/GoogleAppScriptSettings.cs
+++ Editor/Database/GoogleAppScript/GoogleAppScriptSettings.cs
@@ -10,11 +10,23 @@ namespace BorritEditor.Database.GoogleAppScript
         private UserSetting<string> _url = new UserSetting<string>(BorritSettings.Instance, Keys.ScriptUrl, string.Empty, SettingsScope.Project);
         public void OnGUI(string searchContext)
         {
-            EditorGUILayout.LabelField("Google Sheets", EditorStyles.boldLabel);
+            EditorGUILayout.LabelField("Google App Script", EditorStyles.boldLabel);
             EditorGUI.BeginChangeCheck();
             _url.value = SettingsGUILayout.SettingsTextField("Script URL", _url, searchContext);
             
             HasBeenModified = EditorGUI.EndChangeCheck();
+
+            if (HasBeenModified)
+            {
+                if (string.IsNullOrEmpty(_url.value) == false)
+                {
+                    if (_url.value.ToLowerInvariant().StartsWith("https://script.google.com/macros/") == false)
+                    {
+                        _url.value = null;
+                        EditorUtility.DisplayDialog("Error", "Please paste a valid URL to the Google App Script web app", "Ok");
+                    }
+                }
+            }
         }
 
         public static class Keys
